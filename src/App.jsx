a3f91d4f9ac3a06b08c8027b565692b366d3a321
Yuwen-0 <halil.alagul0@gmail.com>
2023-10-23 21:33:16 +0300

    add: Personal Info Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import { useState } from 'react';
 import './App.css'
 import Step from './components/Step';
 function App() {
-
-const Steps = ["YOUR INFO","SELECT PLAN","ADD-ONS","SUMMARY"]
+const [Steps, setSteps] = useState(
+  [
+    {
+      title: 'YOUR INFO',
+      number: 1,
+      isActive: true
+    },
+    {
+      title: 'SELECT PLAN',
+      number: 2,
+      isActive: false
+    },
+    {
+      title: 'ADD-ONS',
+      number: 3,
+      isActive: false
+    },
+    {
+      title: 'SUMMARY',
+      number: 4,
+      isActive: false
+    }
+  ]
+);
 
 
   return (
@@ -11,7 +34,7 @@ const Steps = ["YOUR INFO","SELECT PLAN","ADD-ONS","SUMMARY"]
         <section className='Form-Steps'>
 
           {Steps.map((step, index) => {
-            return <Step key={index} title={step} number={index + 1} />
+            return <Step key={index} title={step.title} isActive={step.isActive} number={index + 1} />
           })}
 
         </section>
@@ -21,7 +44,19 @@ const Steps = ["YOUR INFO","SELECT PLAN","ADD-ONS","SUMMARY"]
             <h1 className='Form-Content-Title'>Personal info</h1>
             <p className='Form-Content-Description'>Please provide your name , email address and phone number.</p>
           </div>
+          <div className='Form-Content-Inputs'>
+            <label htmlFor="name">Name</label>
+              <input type="text" placeholder='e.g. Stephen King' name='name' />
+            <label htmlFor="email">Email</label>
+              <input type="text" placeholder='e.g. 8y8YU@example.com' name='email' />
+            <label htmlFor="phone">Phone Number</label>
+              <input type="text" placeholder='e.g. +1 234 567 890' name='phone' />
+          </div>
+          <section className='Form-Button-Container'>
+            <button className='Form-Button'>Next Step</button>
+          </section>
         </section>
+
       </div>
 
     </>
--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
-export default function Step({title , number}) {
-    return (<div className='Form-Step'>
-          <h1 className='Form-Step-Number'> {number} </h1>
+export default function Step({title , number ,isActive }) {
+    return (<div className="Form-Step">
+          <h1 className={'Form-Step-Number' + (isActive ? " Active":" ")}> {number} </h1>
           <div className='Form-Step-Titles'>
             <p className='Form-Step-Subtitle'>STEP {number}</p>
             <b className='Form-Step-Title'>{title}</b>
